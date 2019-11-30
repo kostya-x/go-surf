87a4e42745fd752ad9f9930f5e6fa2092d5aaae3
Constantine <constantine-x@outlook.com>
2019-12-01 01:41:36 +0200

    slider add slide 1 and set arrows

--- src/js/master.js
+++ src/js/master.js
@@ -1,6 +1,8 @@
 $(function(){
-
-
-
-  
+	$('.header__slider').slick({
+		infinite: true,
+		fade: true,
+		prevArrow: '<img class="slider__arrow slider__arrow--prev" src="img/arrow.svg" alt="previous slide"></img>',
+		nextArrow: '<img class="slider__arrow slider__arrow--next" src="img/arrow.svg" alt="next slide"></img>'
+	});
 });
